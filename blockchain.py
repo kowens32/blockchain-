f1b786957afd0739d4af66386ba1bba3727429fb
Kimberly <kowens4@elon.edu>
2018-06-27 20:47:20 -0400

    the transaction endpoint

--- blockchain.py
+++ blockchain.py
@@ -127,3 +127,18 @@ def full_chain():
 if _name_ == '_main_':
     app.run(host='0.0.0.0', port=5000)
 
+@app/route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    #Check that the required fields are in POST'ed data
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to  Block'{index}}
+    return jsonify(response)
